cd31836aa73193ee65755b4b2743efa0669894b9
liuyang1 <liuyang1@mail.ustc.edu.cn>
2013-12-04 15:12:41 +0800

    fix Bit/s display

--- src/graph.h
+++ src/graph.h
@@ -24,20 +24,21 @@ class Window;
 
 const unsigned long long kilo = 1024;
 const unsigned long long mega = 1024 * 1024;
-const unsigned long long gibi = 1024 * 1024 *1024;
+const unsigned long long giga = 1024 * 1024 * 1024;
 
 static unsigned long long ThresArray[]=
 {0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512,
 	1*kilo, 2*kilo, 4*kilo, 8*kilo, 16*kilo, 32*kilo, 64*kilo, 128*kilo, 256*kilo, 512*kilo,
 	1*mega, 2*mega, 4*mega, 8*mega, 16*mega, 32*mega, 64*mega, 128*mega, 256*mega, 512*mega,
-	1*gibi, 2*gibi, 4*gibi, 8*gibi, 16*gibi, 32*gibi, 64*gibi, 128*gibi, 256*gibi, 512*gibi,
+	1*giga, 2*giga, 4*giga, 8*giga, 16*giga, 32*giga, 64*giga, 128*giga, 256*giga, 512*giga,
 	};
 
-static char ThresStr[][10]=
-{"NULL", "1b","2b","4b","8b","16b","32b","64b","128b","256b","512b",
-	"1Kb","2Kb","4Kb","8Kb","16Kb","32Kb","64Kb","128Kb","256Kb","512Kb",
-	"1Mb","2Mb","4Mb","8Mb","16Mb","32Mb","64Kb","128Kb","256Mb","512Mb",
-	"1Gb","2Gb","4Gb","8Gb","16Gb","32Gb","64Kb","128Kb","256Gb","512Gb",
+static char ThresStrBit[][10]=
+{"NULL","8Bit/s","16Bit/s","32Bit/s","64Bit/s","128Bit/s","256Bit/s","512Bit/s",
+	"1KBit/s","2KBit/s","4KBit/s","8KBit/s","16KBit/s","32KBit/s","64KBit/s","128KBit/s","256KBit/s","512KBit/s",
+	"1MBit/s","2MBit/s","4MBit/s","8MBit/s","16MBit/s","32MBit/s","64KBit/s","128KBit/s","256MBit/s","512MBit/s",
+	"1GBit/s","2GBit/s","4GBit/s","8GBit/s","16GBit/s","32GBit/s","64KBit/s","128KBit/s","256GBit/s","512GBit/s",
+	"1TBit/s","2TBit/s","4TBit/s",
 	};
 	
 inline unsigned long long getThresh(unsigned long long val)
@@ -54,10 +55,10 @@ inline char* getString(unsigned long long val)
 	unsigned int len = sizeof(ThresArray);
 	for (unsigned int i=0; i<len; i++){
 		if (val <= ThresArray[i]){
-			return ThresStr[i];
+			return ThresStrBit[i];
 		}
 	}
-	return ThresStr[0];
+	return ThresStrBit[0];
 }
 
 class Graph
